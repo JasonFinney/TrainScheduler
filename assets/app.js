9be2d5d4869922a02cd412f4746640934190d9a4
Jason Finney <finney.jason.218@gmail.com>
2018-09-10 16:41:50 -0500

    Finished

--- assets/app.js
+++ assets/app.js
@@ -16,6 +16,45 @@ $("#add-train-btn").on("click", function (event) {
 
     database.ref().push(newTrain);
 
+    var longString = moment()._d;
+    var momentapp = longString.toISOString();
+    var currenTime = momentapp.slice(11, 16);
+    var diffTime = moment.utc(moment(currenTime, "HH:mm").diff(moment(time, "HH:mm"))).format("HH:mm");
+    var hours = diffTime.slice(0, 2);
+    var convertedhours = Number(hours) - 5;
+    var minutes = diffTime.slice(3);
+    var minutehours = convertedhours * Number(60);
+    var minutehoursPlus = Number(minutehours) + Number(minutes);
+    var minutehoursDivide = Math.floor(minutehoursPlus / freaquency);
+    var nextTraincoming = minutehoursDivide + 1;
+    var nextTraincoming1 = nextTraincoming * freaquency;
+    var away = nextTraincoming1 - minutehoursPlus;
+    var hoursplace = currenTime.slice(0, 2);
+    var hoursplace1 = Number(hoursplace) - 5;
+    var minutesplace = currenTime.slice(3);
+    var nextTraincomingTime = Number(minutesplace) + Number(away);
+    var hoursplace2 = hoursplace1.toString();
+    var minutesplace1 = nextTraincomingTime.toString();
+    var next = hoursplace2 + ":" + minutesplace1;
+
+    var newTDname = $("<td>");
+    newTDname.append(name);
+    var newTDdestination = $("<td>");
+    newTDdestination.append(destination);
+    var newTDfreaquency = $("<td>");
+    newTDfreaquency.append(freaquency);
+    var newTDnext = $("<td>");
+    newTDnext.append(next);
+    var newTDaway = $("<td>");
+    newTDaway.append(away);
+    var newTR = $("<tr>");
+    newTR.append(newTDname);
+    newTR.append(newTDdestination);
+    newTR.append(newTDfreaquency);
+    newTR.append(newTDnext);
+    newTR.append(newTDaway);
+    $("#train-table").append(newTR);
+
     console.log(newTrain.name);
     console.log(newTrain.destination);
     console.log(newTrain.time);
@@ -30,83 +69,60 @@ $("#add-train-btn").on("click", function (event) {
 
 });
 
+var flag = true;
+
 database.ref().on("value", function (snapshot) {
-    snapshot.forEach(function (childSnapshot) {
-        var data = childSnapshot.val();
-        console.log(data);
-        var name = data.name;
-        console.log(name);
-        var destination = data.destination;
-        console.log(destination);
-        var time = data.time;
-        console.log(time);
-        var freaquency = data.freaquency;
-        console.log(freaquency);
+    if (flag) {
+        snapshot.forEach(function (childSnapshot) {
+            var data = childSnapshot.val();
+            console.log(data);
+            var name = data.name;
+            console.log("Train Name: " + name);
+            var destination = data.destination;
+            console.log("Train Des: " + destination);
+            var time = data.time;
+            console.log("First depart time: " + time);
+            var freaquency = data.freaquency;
+            console.log("Every " + freaquency + "min");
+            var longString = moment()._d;
+            var momentapp = longString.toISOString();
+            var currenTime = momentapp.slice(11, 16);
+            var diffTime = moment.utc(moment(currenTime, "HH:mm").diff(moment(time, "HH:mm"))).format("HH:mm");
+            var hours = diffTime.slice(0, 2);
+            var convertedhours = Number(hours) - 5;
+            var minutes = diffTime.slice(3);
+            var minutehours = convertedhours * Number(60);
+            var minutehoursPlus = Number(minutehours) + Number(minutes);
+            var minutehoursDivide = Math.floor(minutehoursPlus / freaquency);
+            var nextTraincoming = minutehoursDivide + 1;
+            var nextTraincoming1 = nextTraincoming * freaquency;
+            var away = nextTraincoming1 - minutehoursPlus;
+            var hoursplace = currenTime.slice(0, 2);
+            var hoursplace1 = Number(hoursplace) - 5;
+            var minutesplace = currenTime.slice(3);
+            var nextTraincomingTime = Number(minutesplace) + Number(away);
+            var hoursplace2 = hoursplace1.toString();
+            var minutesplace1 = nextTraincomingTime.toString();
+            var next = hoursplace2 + ":" + minutesplace1;
 
-        var diffTime1 = moment().diff(moment(time), "minutes");
-        console.log("let's try this: " + diffTime1);
-        var tRemainder = diffTime1 % freaquency;
-        console.log(tRemainder);
-        var nextTrain = freaquency - tRemainder;
-        console.log(nextTrain);
-        var next1 = moment().add(nextTrain, "minutes");
-        var next = moment(next1).format("hh:mm");
-        console.log(next);
-
-
-        var momentapp = moment().toISOString();
-        var currenTime = momentapp.slice(11, 16);
-        console.log(momentapp);
-        console.log(currenTime);
-        var diffTime = moment.utc(moment(currenTime, "HH:mm").diff(moment(time, "HH:mm"))).format("HH:mm");
-        console.log(diffTime);
-        var hoursTime = time.slice(0, 2);
-        console.log(hoursTime);
-        var hours = diffTime.slice(0, 2);
-        console.log(hours);
-        var minutes = diffTime.slice(3);
-        console.log(minutes);
-        var minutehours = hours * Number(60);
-        console.log(minutehours);
-        var minutehoursPlus = Number(minutehours) + Number(minutes);
-        console.log(minutehoursPlus);
-        var minutehoursRemainder = Math.floor(minutehoursPlus % freaquency);
-        console.log(minutehoursRemainder);
-        var minutehoursDivide = Math.floor(minutehoursPlus / freaquency);
-        console.log(minutehoursDivide);
-        var minutehoursDividePlus = minutehoursDivide + Number(freaquency);
-        console.log(minutehoursDividePlus);
-        var thisIsGettingRidiculous = minutehoursDividePlus * freaquency;
-        console.log(thisIsGettingRidiculous);
-        var thisIsGetting = thisIsGettingRidiculous - Number(minutes);
-        console.log(thisIsGetting);
-        var thisIs = Math.floor(thisIsGetting / Number(60)) + Number(hoursTime);
-        console.log(thisIs);
-
-
-
-        var newTDname = $("<td>");
-        newTDname.append(name);
-        console.log(newTDname);
-        var newTDdestination = $("<td>");
-        newTDdestination.append(destination);
-        console.log(newTDdestination);
-        var newTDfreaquency = $("<td>");
-        newTDfreaquency.append(freaquency);
-        console.log(newTDfreaquency);
-        var newTDnext = $("<td>");
-        newTDnext.append(next);
-        console.log(newTDnext);
-        var newTDaway = $("<td>");
-        newTDaway.append(minutehoursRemainder);
-        console.log(newTDaway);
-        var newTR = $("<tr>");
-        newTR.append(newTDname);
-        newTR.append(newTDdestination);
-        newTR.append(newTDfreaquency);
-        newTR.append(newTDnext);
-        newTR.append(newTDaway);
-        console.log(newTR);
-        $("#train-table").append(newTR);
-    });
+            var newTDname = $("<td>");
+            newTDname.append(name);
+            var newTDdestination = $("<td>");
+            newTDdestination.append(destination);
+            var newTDfreaquency = $("<td>");
+            newTDfreaquency.append(freaquency);
+            var newTDnext = $("<td>");
+            newTDnext.append(next);
+            var newTDaway = $("<td>");
+            newTDaway.append(away);
+            var newTR = $("<tr>");
+            newTR.append(newTDname);
+            newTR.append(newTDdestination);
+            newTR.append(newTDfreaquency);
+            newTR.append(newTDnext);
+            newTR.append(newTDaway);
+            $("#train-table").append(newTR);
+        });
+        flag = false;
+    };
 });
